20c1d2de7138a953afee0b3941f8cfed280743f8
Galaxywalk <rambomail@pku.edu.cn>
2019-12-17 18:12:22 +0800

    IO in websocket

--- Front_End/webtest.py
+++ Front_End/webtest.py
@@ -4,6 +4,32 @@ import logging
 import websockets
 import time
 import database
+import os
+
+#from Microphone import Phone
+#import argparse
+
+# copy from tuochao
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("-d", "--device", type = int, default = 8, help="decice index of microphone")
+# parser.add_argument("-r", "--rate", type = int, default = 48000, help="sampling rate of microphone")
+# parser.add_argument("-c", "--channel", type = int, default = 6, help="the number of channels")
+# parser.add_argument("-w", "--width", type = int, default = 2, help="the size of a single data (Bytes)")
+# parser.add_argument("-l", "--length", type = int, default = 1024, help="The lenght of chunk")
+
+# args = parser.parse_args()
+# RESPEAKER_RATE = args.rate
+# RESPEAKER_CHANNELS = args.channel
+# RESPEAKER_WIDTH = args.width # run getDeviceInfo.py to get index
+# RESPEAKER_INDEX = args.device  # refer to input device id
+# CHUNK = args.length
+# RECORD_SECONDS = 5
+
+
+# microphone = Phone(RESPEAKER_RATE, RESPEAKER_CHANNELS,RESPEAKER_WIDTH,RESPEAKER_INDEX,CHUNK, 1)
+# copy from tuochao
+
 
 logging.basicConfig()
 
@@ -11,6 +37,14 @@ STATE = {"value": 0}
 
 USERS = set()
 
+def writedata(nowtime,data):
+    f = open('.\\keyboard_data\\'+str(nowtime)+'.txt', "a")
+    f.write(str(data['input'])+'\n'+str(data['time']))
+    f.close()
+
+def newfile(nowtime):
+    f = open('.\\keyboard_data\\'+ str(nowtime) +'.txt', 'w')
+    f.close()
 
 def state_event():
     return json.dumps({"type": "state", **STATE})
@@ -26,6 +60,7 @@ def sentence_event():
 async def notify_state():
     if USERS:  # asyncio.wait doesn't accept an empty list
         message = state_event()
+        #await microphone.begin()
         await asyncio.wait([user.send(message) for user in USERS])
 
 
@@ -50,25 +85,33 @@ async def unregister(websocket):
 
 
 async def counter(websocket, path):
+    global nowtime
     # register(websocket) sends user_event() to websocket
     await register(websocket)
     try:
         await websocket.send(state_event())
         async for message in websocket:
             data = json.loads(message)
+            #microphone.end()
+            #microphone.save_file(nowtime+'.wav')
+            writedata(nowtime,data)
             await notify_sentence()
+
             print(data)
             print(time.time())
             # await notify_state()
-
     finally:
         await unregister(websocket)
 
 sentences = database.get_sentences()
 rare_dict = database.generate_rare_dict(sentences)
+nowtime = int(time.time())
+print(nowtime)
+newfile(nowtime)
 print('文字数据库加载完成')
 
 start_server = websockets.serve(counter, "localhost", 6789)
 
+# microphone.begin()
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
